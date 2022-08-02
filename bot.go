cb171aaebe8bb9aa3ef7b7ecf7aec41aa6a8ba6d
Lukas Werling <lukas@lwrl.de>
2022-08-02 23:39:17 +0200

    Implement running commands from the command line

--- bot.go
+++ bot.go
@@ -3,25 +3,26 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 // InitBot sets up and starts the discord bot.
-func InitBot(token string) (*discordgo.Session, error) {
+func InitBot(token string, listen bool) (*discordgo.Session, error) {
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		return nil, err
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
+	if listen {
+		// Register the messageCreate func as a callback for MessageCreate events.
+		dg.AddHandler(messageCreate)
 
-	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
+		// In this example, we only care about receiving message events.
+		dg.Identify.Intents = discordgo.IntentsGuildMessages
+	}
 
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
@@ -42,49 +43,14 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	switch m.Content {
-	case "!get-current-projects":
-		projects, err := getCurrentProjects(s, m.GuildID)
+	case "!get-current-projects",
+		"!get-website-projects",
+		"!check-projects",
+		"!check-releases":
+		res, err := handleCommand(m.Content, s)
 		if err != nil {
 			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("error: %s", err))
 			return
-		}
-		var msg strings.Builder
-		for _, project := range projects {
-			fmt.Fprintf(&msg, "- %s due %s\n", project.ID, project.Deadline.Format("2006-01-02"))
-		}
-		s.ChannelMessageSend(m.ChannelID, msg.String())
-
-	case "!get-website-projects":
-		projects, err := getWebsiteProjects()
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("error: %s", err))
-			return
-		}
-		var msg strings.Builder
-		for _, project := range projects {
-			fmt.Fprintf(&msg, "- %s due %s\n", project.ID, project.Deadline.Format("2006-01-02"))
-		}
-		s.ChannelMessageSend(m.ChannelID, msg.String())
-
-	case "!check-projects":
-		res, err := checkCurrentProjects(s, m.GuildID)
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("error: %s", err))
-			return
-		}
-		if res == "" {
-			res = "All good!"
-		}
-		s.ChannelMessageSend(m.ChannelID, res)
-
-	case "!check-releases":
-		res, err := checkReleases(yt)
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("error: %s", err))
-			return
-		}
-		if res == "" {
-			res = "All good!"
 		}
 		s.ChannelMessageSend(m.ChannelID, res)
 
--- main.go
+++ main.go
@@ -5,38 +5,167 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
+	"github.com/bwmarrin/discordgo"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 )
 
 var yt *youtube.Service
 
+// InitYouTube initializes the YouTube API client.
+func InitYouTube(key string) error {
+	var err error
+	yt, err = youtube.NewService(context.TODO(), option.WithAPIKey(key))
+	return err
+}
+
+func usage() {
+	fmt.Printf("Usage: %s <command>\n", os.Args[0])
+	fmt.Println("Commands:")
+	fmt.Println(" - bot: start the Discord bot")
+	fmt.Println(" - get-current-projects")
+	fmt.Println(" - check-projects")
+	fmt.Println(" - check-releases")
+	fmt.Println(" - get-website-projects")
+}
+
 func main() {
 	token := os.Getenv("DISCORD_TOKEN")
 	youtubeKey := os.Getenv("YOUTUBE_API_KEY")
 
-	var err error
-	yt, err = youtube.NewService(context.TODO(), option.WithAPIKey(youtubeKey))
-	if err != nil {
-		fmt.Println("creating YouTube client failed:", err)
-		return
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
 	}
 
-	dg, err := InitBot(token)
-	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
+	if youtubeKey != "" {
+		if err := InitYouTube(youtubeKey); err != nil {
+			fmt.Println("creating YouTube client failed:", err)
+			return
+		}
 	}
-	defer dg.Close()
 
-	c := InitCron(dg)
-	defer c.Stop()
+	cmd := os.Args[1]
+	switch cmd {
+	case "bot":
+		if yt == nil {
+			fmt.Println("need a YouTube client!")
+			return
+		}
+		dg, err := InitBot(token, true)
+		if err != nil {
+			fmt.Println("error creating Discord session,", err)
+			return
+		}
+		defer dg.Close()
 
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-	<-sc
+		c := InitCron(dg)
+		defer c.Stop()
+
+		// Wait here until CTRL-C or other term signal is received.
+		fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+		<-sc
+
+	// Commands that need a discord session.
+	case "get-current-projects",
+		"check-projects":
+		dg, err := InitBot(token, false)
+		if err != nil {
+			fmt.Println("error creating Discord session,", err)
+			return
+		}
+		defer dg.Close()
+		fmt.Println(cmd)
+
+		res, err := handleCommand("!"+cmd, dg)
+		if err != nil {
+			fmt.Println("error: ", err)
+			return
+		}
+		fmt.Println(res)
+
+	// Commands that need a YouTube client
+	case "check-releases":
+		if yt == nil {
+			fmt.Println("need a YouTube client!")
+			return
+		}
+
+		res, err := handleCommand("!"+cmd, nil)
+		if err != nil {
+			fmt.Println("error: ", err)
+			return
+		}
+		fmt.Println(res)
+
+	// Other commands
+	case "get-website-projects":
+		res, err := handleCommand("!"+cmd, nil)
+		if err != nil {
+			fmt.Println("error: ", err)
+			return
+		}
+		fmt.Println(res)
+
+	default:
+		fmt.Printf("Unknown command %s\n\n", os.Args[1])
+		usage()
+		os.Exit(1)
+	}
+}
+
+// handleCommand handles a bot command, returning the reply.
+func handleCommand(cmd string, dg *discordgo.Session) (string, error) {
+	switch cmd {
+	case "!get-current-projects":
+		projects, err := getCurrentProjects(dg, UVEGuildID)
+		if err != nil {
+			return "", err
+		}
+		var msg strings.Builder
+		for _, project := range projects {
+			fmt.Fprintf(&msg, "- %s due %s\n", project.ID, project.Deadline.Format("2006-01-02"))
+		}
+		return msg.String(), nil
+
+	case "!get-website-projects":
+		projects, err := getWebsiteProjects()
+		if err != nil {
+			return "", err
+		}
+		var msg strings.Builder
+		for _, project := range projects {
+			fmt.Fprintf(&msg, "- %s due %s\n", project.ID, project.Deadline.Format("2006-01-02"))
+		}
+		return msg.String(), nil
+
+	case "!check-projects":
+		res, err := checkCurrentProjects(dg, UVEGuildID)
+		if err != nil {
+			return "", err
+		}
+		if res == "" {
+			res = "All good!"
+		}
+		return res, nil
+
+	case "!check-releases":
+		if yt == nil {
+			return "", fmt.Errorf("no YouTube credentials supplied")
+		}
+		res, err := checkReleases(yt)
+		if err != nil {
+			return "", err
+		}
+		if res == "" {
+			res = "All good!"
+		}
+		return res, nil
+	}
+	return "", fmt.Errorf("unknown command %s", cmd)
 }
